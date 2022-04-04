861c99318911cbb56a162eddd4ef1964c3f4562c
Avishek Jana <avishek@geogo.in>
2022-04-04 15:25:49 +0530

    TODO List  updated with ID

--- todo.rb
+++ todo.rb
@@ -8,7 +8,7 @@ class Todo < ActiveRecord::Base
   def to_displayable_string
     display_status = completed ? "[x]" : "[ ]"
     display_date = due_today? ? nil : due_date
-    "#{display_status} #{todo_text} #{display_date}"
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
   def self.to_displayable_list
